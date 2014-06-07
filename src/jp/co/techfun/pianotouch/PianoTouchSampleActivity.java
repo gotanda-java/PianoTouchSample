f6abd784ad71fb9e699f4416382867578d50fc52
7799 <hayashi7799@gmail.com>
2014-06-07 20:47:53 +0900

    beta.11

--- src/jp/co/techfun/pianotouch/PianoTouchSampleActivity.java
+++ src/jp/co/techfun/pianotouch/PianoTouchSampleActivity.java
@@ -1,6 +1,5 @@
 package jp.co.techfun.pianotouch;
 
-
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.res.Resources;
@@ -19,9 +18,6 @@ import android.view.GestureDetector.SimpleOnGestureListener;
 // ピアノ画面Activity
 public class PianoTouchSampleActivity extends Activity {
 
-    // **GestureDetecotor
-//    public static GestureDetector gesDetect;
-
     // 白鍵鍵盤数を定義する定数
     private static final int WHITE_KEY_NUMBER = 12;
     // 黒鍵鍵盤数を定義する定数
@@ -33,7 +29,7 @@ public class PianoTouchSampleActivity extends Activity {
 
     // **onCreateメソッド(画面初期表示イベント)
     @SuppressLint("Recycle")
-	@Override
+    @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
@@ -41,8 +37,8 @@ public class PianoTouchSampleActivity extends Activity {
         // レイアウトXMLの設定
         setContentView(R.layout.pianotouchsample);
 
-//        // **GestureDetecotorクラスのインスタンス生成
-//        gesDetect = new GestureDetector(this, onGestureListener);
+        // // **GestureDetecotorクラスのインスタンス生成
+        // gesDetect = new GestureDetector(this, onGestureListener);
 
         // 白鍵・黒鍵音声再生用MediaPlayer配列の初期化
         whiteKeyPlayer = new MediaPlayer[WHITE_KEY_NUMBER];
@@ -52,7 +48,7 @@ public class PianoTouchSampleActivity extends Activity {
         TypedArray mids =
             getResources().obtainTypedArray(R.array.mids_whiteKey);
 
-        for (int i = 0; i < mids.length(); i++){
+        for (int i = 0; i < mids.length(); i++) {
 
             int mds = mids.getResourceId(i, -1);
 
@@ -68,7 +64,7 @@ public class PianoTouchSampleActivity extends Activity {
 
         for (int i = 0; i < mids.length(); i++) {
 
-           int mds= mids.getResourceId(i, -1);
+            int mds = mids.getResourceId(i, -1);
 
             if (mds != -1) {
 
@@ -109,17 +105,14 @@ public class PianoTouchSampleActivity extends Activity {
 
         // ボリュームボタンでメディアの音量調整に設定
         setVolumeControlStream(AudioManager.STREAM_MUSIC);
-        
+
     }
 
-    
-    
-    
-    ///////////////////
- // タッチリスナークラス
+    // /////////////////
+    // タッチリスナークラス
     public class ButtonTouchListenerFling implements View.OnTouchListener {
         // GestureDetector生成
-        private GestureDetector gesDetect;        
+        private GestureDetector gesDetect;
         // タッチされたキーに対応する音声フィールド
         private MediaPlayer mp;
 
@@ -131,13 +124,15 @@ public class PianoTouchSampleActivity extends Activity {
 
         // コンストラクタ
         @SuppressWarnings("deprecation")
-        public ButtonTouchListenerFling(MediaPlayer tMp, int tDefaultPic, int tTouchPic) {
-//            GestureDetector gesDetect;
+        public ButtonTouchListenerFling(MediaPlayer tMp, int tDefaultPic,
+            int tTouchPic) {
+            // GestureDetector gesDetect;
             gesDetect = new GestureDetector(onGestureListener);
-             
+
             defaultPic = tDefaultPic;
             touchPic = tTouchPic;
             mp = tMp;
+            // Log.i("Sound",""+tMp);
         }
 
         // onTouchメソッド(タッチイベント)
@@ -146,10 +141,12 @@ public class PianoTouchSampleActivity extends Activity {
         public boolean onTouch(View view, MotionEvent event) {
 
             gesDetect.onTouchEvent(event);
-            Log.i("Sound", view+" + "+event);
-           
+            // Log.i("Sound", view + " + " + event);
+            // Log.i("Sound", ""+view.getId());
+
             // リソースの取得
             Resources res = view.getResources();
+            // Log.i("Sound", "***" + res);
 
             switch (event.getActionMasked()) {
 
@@ -158,8 +155,9 @@ public class PianoTouchSampleActivity extends Activity {
                 // 鍵盤の画像をタッチ時画像に設定
                 view.setBackgroundDrawable(res.getDrawable(touchPic));
                 // 音声を再生
-//                startPlay();
+                // startPlay();
 
+//                Log.i("Sound", "#######" +mp. );
                 break;
 
             // タッチが離れた場合
@@ -167,7 +165,7 @@ public class PianoTouchSampleActivity extends Activity {
                 // 鍵盤の背景をデフォルト画像に設定
                 view.setBackgroundDrawable(res.getDrawable(defaultPic));
                 // 音声を停止
-//                stopPlay();
+                // stopPlay();
 
                 break;
 
@@ -190,8 +188,8 @@ public class PianoTouchSampleActivity extends Activity {
         private void stopPlay() {
             mp.pause();
         }
-        
-     // 複雑なタッチイベントを取得
+
+        // 複雑なタッチイベントを取得
         private final SimpleOnGestureListener onGestureListener =
             new SimpleOnGestureListener() {
                 @Override
@@ -221,9 +219,8 @@ public class PianoTouchSampleActivity extends Activity {
                     float velocityX, float velocityY) {
                     // TODO Auto-generated method stub
                     Log.v("Gesture", "onFling");
-                    
-//                    startPlay();
-                    
+
+                    // startPlay();
                     return super.onFling(e1, e2, velocityX, velocityY);
                 }
 
@@ -238,16 +235,33 @@ public class PianoTouchSampleActivity extends Activity {
                 @Override
                 public boolean onScroll(MotionEvent e1, MotionEvent e2,
                     float distanceX, float distanceY) {
-                    // TODO Auto-generated method stub
-                    Log.v("Gesture", "onScroll");
-                    Log.v("Gesture", e1+"+"+e2);
-                    startPlay();
+                    float dis = 0;
+                    dis = distanceX - distanceY;
+                    if (dis <= 0 && dis <= -20) {
+//                        blackKeyPlayer[5].seekTo(0);
+                        blackKeyPlayer[5].start();
+
                         try {
-                            Thread.currentThread().sleep(90);
+                            Thread.currentThread().sleep(100);
                         } catch (InterruptedException e) {
                             // TODO Auto-generated catch block
                             e.printStackTrace();
                         }
+//                        blackKeyPlayer[6].seekTo(0);
+                        blackKeyPlayer[6].start();
+                    } else if (dis >= 0 && dis >= 20) {
+//                        blackKeyPlayer[3].seekTo(0);
+                        blackKeyPlayer[3].start();
+
+                        try {
+                            Thread.currentThread().sleep(100);
+                        } catch (InterruptedException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+//                        blackKeyPlayer[2].seekTo(0);
+                        blackKeyPlayer[2].start();
+                    }
                     return super.onScroll(e1, e2, distanceX, distanceY);
                 }
 
@@ -274,16 +288,14 @@ public class PianoTouchSampleActivity extends Activity {
             };
 
     }
-    
-    
 
-//    @Override
-//    public boolean onTouchEvent(MotionEvent event) {
-//        // TODO Auto-generated method stub
-//        // **タッチイベントをGestureDetector#onTouchEventメソッドに
-//        gesDetect.onTouchEvent(event);
-//        return false;
-//    }
+    // @Override
+    // public boolean onTouchEvent(MotionEvent event) {
+    // // TODO Auto-generated method stub
+    // // **タッチイベントをGestureDetector#onTouchEventメソッドに
+    // gesDetect.onTouchEvent(event);
+    // return false;
+    // }
 
     // onDestroyメソッド(アクティビティ破棄イベント)
     @Override
@@ -306,5 +318,4 @@ public class PianoTouchSampleActivity extends Activity {
         }
     }
 
-    
 }
